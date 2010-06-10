b8502a94161096f835211e5e8f1ca872957833ed
Nathan Neff <nate@lemmy2.local>
2010-06-10 12:23:09 -0500

    doc.sh works from any dir, and works w/mac.

--- bundle/doc.sh
+++ bundle/doc.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 # Indexes the documentation in the bundles in this directory.
+MYDIR=`dirname $0`
+cd $MYDIR
 for dir in */doc/; do
-  vim -u NONE -c "helptags $dir" -c quit
+    if uname -a | grep --silent -i 'Darwin'
+    then
+        mvim -u NONE -c "helptags $dir" -c quit
+    else
+        vim -u NONE -c "helptags $dir" -c quit
+    fi
 done
